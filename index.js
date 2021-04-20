c079f30c860daa4968558e5edee6842ca7f94be9
Cole Kendall <cole.kendall.18@cnu.edu>
2021-04-20 16:50:07 -0400

    add empty functions for a RustCiv

---
+++ index.js
@@ -0,0 +1,26 @@
+function RustCiv() {
+  //Highest level object for a rust clan
+  //Every RustCiv should have an Identity,
+  //Loot, Base, Relations
+}
+
+function Identity() {
+  //Each rust civ should have a name,
+  //a set of members, and possibly more.
+}
+
+function Member() {
+  //Each member should have a skillset, name, and possibly more
+}
+
+function Loot() {
+  //Raw reasources, comps, weapons, ammor, meds, ect.
+}
+
+function Base() {
+  //Base type, size, storage
+}
+
+function Relations() {
+  //relations ships with other clans
+}
