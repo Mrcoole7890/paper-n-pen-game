5feafc10aa714e2c4036bbd11e7630e2edc96855
Cole Kendall <cole.kendall.18@cnu.edu>
2021-04-23 17:22:44 -0400

    Implmented Loot object

--- index.js
+++ index.js
@@ -72,19 +72,88 @@ function Loot() {
   this.getMeds = function() {return this.meds;}
 
   //ToDo: implement these
-  this.addRawResources = function(nameOfResource, quantity) {}
-  this.addComponets = function(nameOfComponet, quantity) {}
-  this.addWeapon = function(weapon) {}
-  this.addAmmor = function(ammor) {}
-  this.addMeds = function(nameOfMeds, quantity) {}
+  this.addRawResources = function(nameOfResource, quantity) {
+    for(var i = 0; i < this.rawReasources.length; i++){
+      if (this.rawReasources[i][0] == nameOfResource) {
+          this.rawReasources[i][1] += quantity;
+          return;
+      }
+    }
+    this.rawReasources.push([nameOfResource, quantity]);
+  };
+  this.addComponets = function(nameOfComponet, quantity) {
+    for(var i = 0; i < this.componets.length; i++){
+      if (this.componets[i][0] == nameOfResource) {
+          this.componets[i][1] += quantity;
+          return;
+      }
+    }
+    this.componets.push([nameOfResource, quantity]);
+  };
+  this.addWeapon = function(weapon) {this.weapons.push(weapon)};
+  this.addAmmor = function(ammor) {this.ammor.push(ammor)};
+  this.addMeds = function(nameOfMeds, quantity) {
+    for(var i = 0; i < this.meds.length; i++){
+      if (this.meds[i][0] == nameOfResource) {
+          this.meds[i][1] += quantity;
+          return;
+      }
+    }
+    this.meds.push([nameOfResource, quantity]);
+  }
 
   //ToDo: implement these
-  this.removeRawResources = function(nameOfResource, quantity) {}
-  this.removeComponets = function(nameOfComponet, quantity) {}
-  this.removeWeapon = function(idOfWeapon) {}
-  this.removeMeds = function(nameOfMeds, quantity) {}
-  this.removeAmmor = function(idOfAmmor) {}
-
+  this.removeRawResources = function(nameOfResource, quantity) {
+    for(var i = 0; i < this.meds.length; i++){
+      if (this.meds[i][0] == nameOfResource) {
+          if( this.meds[i][1] - quantity < 0)
+              return "Not Enough" + nameOfResource;
+          this.med[i][1] -= quantity;
+          return;
+      }
+    }
+    return "No " + nameOfResource + "ready";
+  }
+  this.removeComponets = function(nameOfComponet, quantity) {
+    for(var i = 0; i < this.componets.length; i++){
+      if (this.componets[i][0] == nameOfComponet) {
+          if( this.componets[i][1] - quantity < 0)
+              return "Not Enough" + nameOfComponet;
+          this.componets[i][1] -= quantity;
+          return;
+      }
+    }
+    return "No " + nameOfComponet + "ready";
+  }
+  this.removeWeapon = function(idOfWeapon) {
+    for(var i = 0; i < this.weapons.length; i++){
+      if (this.weapons[i].getID() == idOfWeapon) {
+        this.weapons.splice(i,1);
+        return;
+      }
+    }
+    return "Weapon id:" + idOfWeapon + "not found";
+  }
+  this.removeMeds = function(nameOfMeds, quantity) {
+    for(var i = 0; i < this.meds.length; i++){
+      if (this.meds[i][0] == nameOfMeds) {
+          if( this.componets[i][1] - quantity < 0)
+              return "Not Enough" + nameOfMeds;
+          this.meds[i][1] -= quantity;
+          return;
+      }
+    }
+    return "No " + nameOfMeds + "ready";
+  }
+  this.removeAmmor = function(idOfAmmor) {
+    for(var i = 0; i < this.ammor.length; i++){
+      if (this.ammor[i].getID() == idOfWeapon) {
+        this.ammor.splice(i,1);
+        return;
+      }
+    }
+    return "Ammor id:" + idOfAmmor + "not found";
+  }
 }
 
 function Base() {
@@ -106,5 +175,5 @@ function Base() {
 
 function Relations() {
   //relations ships with other clans
-  this.clans;
+  this.clans; //List Of Clans and their accociated feeling towards this clan
 }
