fc01386cb6c6255c1233a08055af578b1236d9fe
Cole Kendall <cole.kendall.18@cnu.edu>
2021-04-23 06:16:52 -0400

    Add getters and setters for Loot class

--- index.js
+++ index.js
@@ -7,14 +7,74 @@ function RustCiv() {
 function Identity() {
   //Each rust civ should have a name,
   //a set of members, and possibly more.
+  this.members;
+  this.name;
+
+  this.getMembers = function() {
+    return this.members;
+  };
+
+  this.addMember = function(member) {
+    this.members[this.members.length] = member;
+  };
+
+  this.removeMember = function(name) {
+    for(var i = 0; i < this.members.length; i++){
+      if (this.members[i].getName == name) {
+        this.members.splice(i,1);
+      }
+    }
+  };
+
+  this.getName = function() {
+    return this.name;
+  }
+
+  this.setName = function(name) {
+    this.name = name;
+  }
 }
 
 function Member() {
   //Each member should have a skillset, name, and possibly more
+  this.skillset;
+  this.name;
+
+  this.setSkillSet  = function(skillset) {
+    this.skillset = skillset;
+  };
+
+  this.getSkillSet = function() {
+    return this.skillset;
+  };
+
+  this.setName = function(name) {
+    this.name = name;
+  };
+
+  this.getName = function() {
+    return this.name;
+  };
 }
 
 function Loot() {
   //Raw reasources, comps, weapons, ammor, meds, ect.
+  this.rawReasources; // 2d array of reasources names and their quantity
+  this.componets; // 2d array of componet names and their quantity
+  this.weapons; // array of weapon objects
+  this.ammor; // array of ammor objects
+  this.meds; // 2d array of meds and their quantity
+
+  this.getRawResources = function() {return this.rawReasources;}
+  this.getComponets = function() {return this.componets;}
+  this.getWeapons = function() {return this.weapons;}
+  this.getAmmor = function() {return this.ammor;}
+  this.getMeds = function() {return this.meds;}
+
+  this.addRawResources = function(nameOfResource, quantity) {}
+  this.addComponets = function(nameOfComponet, quantity) {}
+  this.addWeapon = function(weapon) {}
+  this.addMeds = function(nameOfMeds, quantity) {}
 }
 
 function Base() {
