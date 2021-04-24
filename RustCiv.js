249bb99eb9ef95f249bf6fe0ab39d17a83335586
Cole Kendall <cole.kendall.18@cnu.edu>
2021-04-24 11:03:37 -0400

    Add html file for testing RustCiv class

---
+++ RustCiv.js
@@ -0,0 +1,197 @@
+function RustCiv(identityObj, lootObj, baseObj, relationsObj) {
+  //Highest level object for a rust clan
+  //Every RustCiv should have an Identity,
+  //Loot, Base, Relations
+
+  this.identity = identityObj;
+  this.loot = lootObj;
+  this.base = baseObj;
+  this.relations = relationsObj;
+
+}
+
+function Identity() {
+  //Each rust civ should have a name,
+  //a set of members, and possibly more.
+  this.members = [];
+  this.name;
+
+  this.getMembers = function() {
+    return this.members;
+  };
+
+  this.addMember = function(member) {
+    this.members[this.members.length] = member;
+  };
+
+  this.removeMember = function(name) {
+    for(var i = 0; i < this.members.length; i++){
+      if (this.members[i].getName == name) {
+        this.members.splice(i,1);
+      }
+    }
+  };
+
+  this.getName = function() {
+    return this.name;
+  }
+
+  this.setName = function(name) {
+    this.name = name;
+  }
+}
+
+function Member() {
+  //Each member should have a skillset, name, and possibly more
+  this.skillset;
+  this.name;
+
+  this.setSkillSet  = function(skillset) {
+    this.skillset = skillset;
+  };
+
+  this.getSkillSet = function() {
+    return this.skillset;
+  };
+
+  this.setName = function(name) {
+    this.name = name;
+  };
+
+  this.getName = function() {
+    return this.name;
+  };
+}
+
+function Loot() {
+  //Raw reasources, comps, weapons, ammor, meds, ect.
+  this.rawReasources = []; // 2d array of reasources names and their quantity
+  this.componets = []; // 2d array of componet names and their quantity
+  this.weapons = []; // array of weapon objects
+  this.ammor = []; // array of ammor objects
+  this.meds = []; // 2d array of meds and their quantity
+
+  this.getRawResources = function() {return this.rawReasources;}
+  this.getComponets = function() {return this.componets;}
+  this.getWeapons = function() {return this.weapons;}
+  this.getAmmor = function() {return this.ammor;}
+  this.getMeds = function() {return this.meds;}
+
+  //ToDo: implement these
+  this.addRawResources = function(nameOfResource, quantity) {
+    for(var i = 0; i < this.rawReasources.length; i++){
+      if (this.rawReasources[i][0] == nameOfResource) {
+          this.rawReasources[i][1] += quantity;
+          return;
+      }
+    }
+    this.rawReasources.push([nameOfResource, quantity]);
+  };
+  this.addComponets = function(nameOfComponet, quantity) {
+    for(var i = 0; i < this.componets.length; i++){
+      if (this.componets[i][0] == nameOfResource) {
+          this.componets[i][1] += quantity;
+          return;
+      }
+    }
+    this.componets.push([nameOfResource, quantity]);
+  };
+  this.addWeapon = function(weapon) {this.weapons.push(weapon)};
+  this.addAmmor = function(ammor) {this.ammor.push(ammor)};
+  this.addMeds = function(nameOfMeds, quantity) {
+    for(var i = 0; i < this.meds.length; i++){
+      if (this.meds[i][0] == nameOfResource) {
+          this.meds[i][1] += quantity;
+          return;
+      }
+    }
+    this.meds.push([nameOfResource, quantity]);
+  }
+
+  //ToDo: implement these
+  this.removeRawResources = function(nameOfResource, quantity) {
+    for(var i = 0; i < this.meds.length; i++){
+      if (this.meds[i][0] == nameOfResource) {
+          if( this.meds[i][1] - quantity < 0)
+              return "Not Enough" + nameOfResource;
+          this.med[i][1] -= quantity;
+          return;
+      }
+    }
+    return "No " + nameOfResource + "ready";
+  }
+  this.removeComponets = function(nameOfComponet, quantity) {
+    for(var i = 0; i < this.componets.length; i++){
+      if (this.componets[i][0] == nameOfComponet) {
+          if( this.componets[i][1] - quantity < 0)
+              return "Not Enough" + nameOfComponet;
+          this.componets[i][1] -= quantity;
+          return;
+      }
+    }
+    return "No " + nameOfComponet + "ready";
+  }
+  this.removeWeapon = function(idOfWeapon) {
+    for(var i = 0; i < this.weapons.length; i++){
+      if (this.weapons[i].getID() == idOfWeapon) {
+        this.weapons.splice(i,1);
+        return;
+      }
+    }
+    return "Weapon id:" + idOfWeapon + "not found";
+  }
+  this.removeMeds = function(nameOfMeds, quantity) {
+    for(var i = 0; i < this.meds.length; i++){
+      if (this.meds[i][0] == nameOfMeds) {
+          if( this.componets[i][1] - quantity < 0)
+              return "Not Enough" + nameOfMeds;
+          this.meds[i][1] -= quantity;
+          return;
+      }
+    }
+    return "No " + nameOfMeds + "ready";
+  }
+  this.removeAmmor = function(idOfAmmor) {
+    for(var i = 0; i < this.ammor.length; i++){
+      if (this.ammor[i].getID() == idOfWeapon) {
+        this.ammor.splice(i,1);
+        return;
+      }
+    }
+    return "Ammor id:" + idOfAmmor + "not found";
+  }
+}
+
+function Base() {
+  //Base type, size, storage
+  this.type;
+  this.size;
+  this.storage;
+
+  //ToDo: implement these
+  this.getType = function() {
+    return this.type;
+  }
+  this.getSize = function() {
+    return this.size;
+  }
+  this.getStorage = function() {
+    return this.storage;
+  }
+
+  //ToDo: implement these
+  this.setType = function(type) {
+    this.type = type;
+  }
+  this.setSize = function(size) {
+    this.size = size;
+  }
+  this.getStorage = function(storage) {
+    this.storage = storage;
+  }
+}
+
+function Relations() {
+  //relations ships with other clans
+  this.clans; //List Of Clans and their accociated feeling towards this clan
+}
--- index.js
+++ index.js
@@ -1,191 +1,40 @@
-function RustCiv() {
-  //Highest level object for a rust clan
-  //Every RustCiv should have an Identity,
-  //Loot, Base, Relations
-}
+var ClanElement = document.getElementById("Clan");
+var MembersElement = document.getElementById("Members");
+var LootElement = document.getElementById("Loot");
+var BaseElement = document.getElementById("Base");
+var RelationsElement = document.getElementById("Relations");
+var InfoElement = document.getElementById("Info");
 
-function Identity() {
-  //Each rust civ should have a name,
-  //a set of members, and possibly more.
-  this.members;
-  this.name;
+var TestClanIdentity = new Identity();
+var TestClanLoot = new Loot();
+var TestClanBase = new Base();
+var TestClanRelations = new Relations();
 
-  this.getMembers = function() {
-    return this.members;
-  };
+var TestClanMemberOne = new Member();
+var TestClanMemberTwo = new Member();
 
-  this.addMember = function(member) {
-    this.members[this.members.length] = member;
-  };
+TestClanIdentity.setName("Cum Gobblins");
 
-  this.removeMember = function(name) {
-    for(var i = 0; i < this.members.length; i++){
-      if (this.members[i].getName == name) {
-        this.members.splice(i,1);
-      }
-    }
-  };
+TestClanMemberOne.setName("NOT_COLE");
+TestClanMemberOne.setSkillSet("Farming");
 
-  this.getName = function() {
-    return this.name;
+TestClanMemberTwo.setName("Senpai Hamma");
+TestClanMemberTwo.setSkillSet("PvP");
+
+TestClanIdentity.addMember(TestClanMemberOne);
+TestClanIdentity.addMember(TestClanMemberTwo);
+
+var TestClan = new RustCiv(TestClanIdentity,TestClanLoot,TestClanBase,TestClanRelations);
+
+ClanElement.onclick = function() {
+  InfoElement.innerHTML = "";
+  var clanName = document.createElement("H4");
+  clanName.innerHTML = "Clan Name:<input class=\"form-control\" type=\"text\" placeholder=\"" + TestClan.identity.getName() + "\" readonly>";
+  InfoElement.appendChild(clanName);
+  for (var i = 0; i < TestClan.identity.getMembers().length; i++) {
+    var tempMember = document.createElement("Button");
+    tempMember.setAttribute("class", "btn btn-outline-primary")
+    tempMember.innerHTML = TestClan.identity.getMembers()[i].getName();
+    InfoElement.appendChild(tempMember);
   }
-
-  this.setName = function(name) {
-    this.name = name;
-  }
-}
-
-function Member() {
-  //Each member should have a skillset, name, and possibly more
-  this.skillset;
-  this.name;
-
-  this.setSkillSet  = function(skillset) {
-    this.skillset = skillset;
-  };
-
-  this.getSkillSet = function() {
-    return this.skillset;
-  };
-
-  this.setName = function(name) {
-    this.name = name;
-  };
-
-  this.getName = function() {
-    return this.name;
-  };
-}
-
-function Loot() {
-  //Raw reasources, comps, weapons, ammor, meds, ect.
-  this.rawReasources; // 2d array of reasources names and their quantity
-  this.componets; // 2d array of componet names and their quantity
-  this.weapons; // array of weapon objects
-  this.ammor; // array of ammor objects
-  this.meds; // 2d array of meds and their quantity
-
-  this.getRawResources = function() {return this.rawReasources;}
-  this.getComponets = function() {return this.componets;}
-  this.getWeapons = function() {return this.weapons;}
-  this.getAmmor = function() {return this.ammor;}
-  this.getMeds = function() {return this.meds;}
-
-  //ToDo: implement these
-  this.addRawResources = function(nameOfResource, quantity) {
-    for(var i = 0; i < this.rawReasources.length; i++){
-      if (this.rawReasources[i][0] == nameOfResource) {
-          this.rawReasources[i][1] += quantity;
-          return;
-      }
-    }
-    this.rawReasources.push([nameOfResource, quantity]);
-  };
-  this.addComponets = function(nameOfComponet, quantity) {
-    for(var i = 0; i < this.componets.length; i++){
-      if (this.componets[i][0] == nameOfResource) {
-          this.componets[i][1] += quantity;
-          return;
-      }
-    }
-    this.componets.push([nameOfResource, quantity]);
-  };
-  this.addWeapon = function(weapon) {this.weapons.push(weapon)};
-  this.addAmmor = function(ammor) {this.ammor.push(ammor)};
-  this.addMeds = function(nameOfMeds, quantity) {
-    for(var i = 0; i < this.meds.length; i++){
-      if (this.meds[i][0] == nameOfResource) {
-          this.meds[i][1] += quantity;
-          return;
-      }
-    }
-    this.meds.push([nameOfResource, quantity]);
-  }
-
-  //ToDo: implement these
-  this.removeRawResources = function(nameOfResource, quantity) {
-    for(var i = 0; i < this.meds.length; i++){
-      if (this.meds[i][0] == nameOfResource) {
-          if( this.meds[i][1] - quantity < 0)
-              return "Not Enough" + nameOfResource;
-          this.med[i][1] -= quantity;
-          return;
-      }
-    }
-    return "No " + nameOfResource + "ready";
-  }
-  this.removeComponets = function(nameOfComponet, quantity) {
-    for(var i = 0; i < this.componets.length; i++){
-      if (this.componets[i][0] == nameOfComponet) {
-          if( this.componets[i][1] - quantity < 0)
-              return "Not Enough" + nameOfComponet;
-          this.componets[i][1] -= quantity;
-          return;
-      }
-    }
-    return "No " + nameOfComponet + "ready";
-  }
-  this.removeWeapon = function(idOfWeapon) {
-    for(var i = 0; i < this.weapons.length; i++){
-      if (this.weapons[i].getID() == idOfWeapon) {
-        this.weapons.splice(i,1);
-        return;
-      }
-    }
-    return "Weapon id:" + idOfWeapon + "not found";
-  }
-  this.removeMeds = function(nameOfMeds, quantity) {
-    for(var i = 0; i < this.meds.length; i++){
-      if (this.meds[i][0] == nameOfMeds) {
-          if( this.componets[i][1] - quantity < 0)
-              return "Not Enough" + nameOfMeds;
-          this.meds[i][1] -= quantity;
-          return;
-      }
-    }
-    return "No " + nameOfMeds + "ready";
-  }
-  this.removeAmmor = function(idOfAmmor) {
-    for(var i = 0; i < this.ammor.length; i++){
-      if (this.ammor[i].getID() == idOfWeapon) {
-        this.ammor.splice(i,1);
-        return;
-      }
-    }
-    return "Ammor id:" + idOfAmmor + "not found";
-  }
-}
-
-function Base() {
-  //Base type, size, storage
-  this.type;
-  this.size;
-  this.storage;
-
-  //ToDo: implement these
-  this.getType = function() {
-    return this.type;
-  }
-  this.getSize = function() {
-    return this.size;
-  }
-  this.getStorage = function() {
-    return this.storage;
-  }
-
-  //ToDo: implement these
-  this.setType = function(type) {
-    this.type = type;
-  }
-  this.setSize = function(size) {
-    this.size = size;
-  }
-  this.getStorage = function(storage) {
-    this.storage = storage;
-  }
-}
-
-function Relations() {
-  //relations ships with other clans
-  this.clans; //List Of Clans and their accociated feeling towards this clan
 }
